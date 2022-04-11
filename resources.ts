b2c2b20dab4a96e26968e9282817f84fdbdeec9a
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-04-11 16:28:07 +0200

    refactor: use common code for all queries

--- resources.ts
+++ resources.ts
@@ -1,6 +1,6 @@
 import { NamedNode, Term, DatasetCoreFactory } from 'rdf-js'
 import clownface, { GraphPointer } from 'clownface'
-import { Construct, DESCRIBE, sparql } from '@tpluscode/sparql-builder'
+import { Construct, DESCRIBE } from '@tpluscode/sparql-builder'
 import { sh } from '@tpluscode/rdf-ns-builders/strict'
 import { toSparql } from 'clownface-shacl-path'
 import { StreamClient } from 'sparql-http-client'
@@ -36,44 +36,36 @@ interface Children {
   execute(client: StreamClient, rdf: DatasetCoreFactory): Promise<GraphPointer[]>
 }
 
-export function children(level: GraphPointer, parent: Term, { limit = 1, offset = 0, orderBy = [] }: ChildrenOptions = {}): Children {
+export function children(level: GraphPointer, parent: Term, { limit = 1, offset = 0, orderBy = [] }: ChildrenOptions = {}): Children | null {
+  const patterns = getHierarchyPatterns(level, {
+    firstLevel: requiredPath,
+  })
+  if (!patterns) {
+    return null
+  }
   const path = level.out(sh.path)
 
-  const targetClass = level.out(sh.targetClass).term
-  let typePattern = sparql``
-  if (targetClass) {
-    typePattern = sparql`?this a ${targetClass} .`
-  }
-
-  const orderPatterns = orderBy.reduce((prev, property, index) => {
-    return sparql`${prev}
-    OPTIONAL {
-      ?this ${property} ${variable(`order${index}`)}
-    }`
-  }, sparql``)
-
-  let query = DESCRIBE`${parent} ?this`
+  const query = DESCRIBE`${parent} ?this`
     .WHERE`
       ${parent} ${toSparql(path)} ?this .
-      ${typePattern}
-      ${orderPatterns}
+      ${patterns}
 
       filter(isiri(?this))
     `
     .LIMIT(limit)
     .OFFSET(offset)
 
-  orderBy.forEach((property, index) => {
+  const orderedQuery = orderBy.reduce((query, property, index) => {
     const orderVar = variable(`order${index}`)
-    query = query
+    return query
       .WHERE`OPTIONAL { ?this ${property} ${orderVar} }`
       .ORDER().BY(orderVar)
-  })
+  }, query)
 
   return {
-    query,
+    query: orderedQuery,
     execute: async function (client: StreamClient, $rdf: DatasetCoreFactory) {
-      const stream = await query.execute(client.query)
+      const stream = await orderedQuery.execute(client.query)
       const dataset = await fromStream($rdf.dataset(), stream)
 
       const parentNode = clownface({ dataset, term: parent })
